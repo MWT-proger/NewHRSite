1986a1e28b82781e44d9eee8053e438568368ec0
MWT-proger <dry.wats@gmail.com>
2022-02-04 18:04:03 +0300

    Доделал дизайн авторизации

--- config/static/new_assets/js/form-validation.js
+++ config/static/new_assets/js/form-validation.js
@@ -7,37 +7,29 @@ $(function() {
     }
   });
   $(function() {
-    // validate signup form on keyup and submit
+    // validate login form on keyup and submit
     $("#loginModalForm").validate({
       rules: {
         loginNumberPhone: {
-          required: true
+          required: true,
+          checkMaskPhone: true
 
         },
         loginPassword: {
           required: true,
           minlength: 8
-        },
-        // confirm_password: {
-        //   required: true,
-        //   minlength: 8,
-        //   equalTo: "#password"
-        // },
+        }
       },
       messages: {
         loginNumberPhone: {
           required: "Пожалуйста, введите номер телефона",
+          checkMaskPhone: "Пожалуйста, введите правильный номер телефона",
           minlength: "Пожалуйста, введите правильный номер телефона"
         },
         loginPassword: {
           required: "Пожалуйста, введите пароль",
           minlength: "Ваш пароль должен быть не менее 8 символов"
-        },
-        // confirm_password: {
-        //   required: "Пожалуйста, введите пароль",
-        //   minlength: "Ваш пароль должен быть не менее 8 символов",
-        //   equalTo: "Пожалуйста, введите тот же пароль, что и выше"
-        // }
+        }
       },
       errorPlacement: function(label, element) {
         label.addClass('mt-2 text-danger');
@@ -53,6 +45,9 @@ $(function() {
     // validate signup form on keyup and submit
     $("#signUpModalForm").validate({
       rules: {
+        signUpNameCompany: {
+          required: true
+        },
         signUpNumberPhone: {
           required: true,
           checkMaskPhone: true
@@ -68,12 +63,15 @@ $(function() {
         signUpConfirmPassword: {
           required: true,
           minlength: 8,
-          equalTo: "#password"
+          equalTo: "#signUpPassword"
         },
 
 
       },
       messages: {
+        signUpNameCompany: {
+          required: "Пожалуйста, введите название компании"
+        },
         signUpNumberPhone: {
           required: "Пожалуйста, введите номер телефона",
           checkMaskPhone: "Пожалуйста, введите правильный номер телефона",
@@ -89,6 +87,32 @@ $(function() {
           equalTo: "Пожалуйста, введите тот же пароль, что и выше"
         },
         signUpEmail: "Пожалуйста, введите действительный адрес электронной почты",
+      },
+      errorPlacement: function(label, element) {
+        label.addClass('mt-2 text-danger');
+        label.insertAfter(element);
+      },
+      highlight: function(element, errorClass) {
+        $(element).parent().addClass('has-danger');
+        $(element).addClass('form-control-danger');
+      }
+    });
+  });
+  $(function() {
+    // validate signup form on keyup and submit
+    $("#forgotPasswordModalForm").validate({
+      rules: {
+        forgotPasswordNumberPhone: {
+          required: true,
+          checkMaskPhone: true
+        }
+      },
+      messages: {
+        forgotPasswordNumberPhone: {
+          required: "Пожалуйста, введите номер телефона",
+          checkMaskPhone: "Пожалуйста, введите правильный номер телефона",
+          minlength: "Пожалуйста, введите правильный номер телефона"
+        }
       },
       errorPlacement: function(label, element) {
         label.addClass('mt-2 text-danger');
